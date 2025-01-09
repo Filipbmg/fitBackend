d04e9659495c194f415011c3c507b1c86ac34db1
Filip Gynthersen <filipg@live.dk>
2025-01-09 22:47:35 +0100

    update db structure

--- database/setupDatabase.js
+++ database/setupDatabase.js
@@ -9,6 +9,14 @@ async function setupDatabase() {
         password VARCHAR(255) NOT NULL
       );
     `);
+    await db.execute(`
+      CREATE TABLE IF NOT EXISTS workout_routines (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT NOT NULL UNIQUE,
+        routine_data MEDIUMTEXT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+      );
+    `);
     console.log("Database setup complete");
   } catch (err) {
     console.error("Error setting up the database: ", err.message);
