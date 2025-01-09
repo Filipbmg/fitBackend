ad0eeb10c88518b1decff4146fdbd22c87112aba
Filip Gynthersen <filipg@live.dk>
2025-01-09 22:48:51 +0100

    restructure

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ import cors from "cors";
 import helmet from "helmet";
 import { globalErrorHandler } from "./middleware/errorHandlers.js";
 import authRouter from "./routers/authRouter.js";
+import contentRouter from "./routers/contentRouter.js";
 import { db } from "./database/connection.js";
 
 dotenv.config();
 
 const app = express();
-app.use(globalErrorHandler);
 app.use(cors({
     credentials: true,
     origin: true
@@ -37,13 +37,15 @@ app.use(session({
 }));
 
 app.use(authRouter);
+app.use(contentRouter);
 
 app.all("*", (req, res, next) => {
-    const err = new Error(`Can't find ${req.originalUrl}`);
+    const err = new Error(`Kan ikke finde: ${req.originalUrl}`);
     err.status = 404;
 
     next(err);
 });
+app.use(globalErrorHandler);
 
 const PORT = 8080;
 app.listen(PORT, () => {
